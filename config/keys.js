94cd141fde10f0fb010e23b4e472ac725ea0dd8f
RafayFarrukh <rafayfarrukh941@gmail.com>
2022-03-19 01:53:24 +0500

    first commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://rafay:rafay123@cluster0-shard-00-00.4wdzd.mongodb.net:27017,cluster0-shard-00-01.4wdzd.mongodb.net:27017,cluster0-shard-00-02.4wdzd.mongodb.net:27017/Dev_connector?ssl=true&replicaSet=atlas-65ycc6-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
